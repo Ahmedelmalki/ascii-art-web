331f29556380d63e73b471f47b3fa1d1456dcca9
aelmalki <elmalkiahmed02@gmail.com>
2024-06-09 05:04:28 +0100

    jun 9 at 5 am

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func main() {
+	http.HandleFunc("/home",
+		func(w http.ResponseWriter, r *http.Request) {
+			tpl, _ = template.ParseFiles("templates/home.html")
+			tpl.Execute(w, nil)
+		})
+
+	http.ListenAndServe(":8000", http.DefaultServeMux)
+}
+ 
